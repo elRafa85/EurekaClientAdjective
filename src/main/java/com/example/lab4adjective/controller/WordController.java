a714040854612f9274ae14b8a9b2ffa28e45b39e
Rafael Ramirez Lozano <rafaelrl85@hotmail.com>
2017-10-16 00:29:20 -0500

    This is the client for the adjective

---
+++ src/main/java/com/example/lab4adjective/controller/WordController.java
@@ -0,0 +1,21 @@
+package com.example.lab4adjective.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+public class WordController {
+
+    @Value("${words}")
+    String words;
+
+    public @ResponseBody String getWords(){
+
+        String[] listOfWords = words.split(",");
+        return Arrays.stream(listOfWords).findAny().get();
+    }
+
+}
